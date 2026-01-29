276ace1668b852e06d899fd9ea3ed463a76661bb
blake12332323232 <110481824+blake12332323232@users.noreply.github.com>
2026-01-28 19:59:55 -0600

    Add Keepalive server to respond with 'Bot is alive!'

---
+++ Keepalive.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log("KeepAlive server is running on port 3000");
+  });
+}
+
+module.exports = keepAlive;
